6525debd22bacbe97a000ae07f26c06cf829e13d
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2021-02-16 22:09:14 +0100

    Drop Python 3.5, support Python 3.8 and 3.9

--- setup.py
+++ setup.py
@@ -24,9 +24,10 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         # 'License :: ,
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords=[
     ],
